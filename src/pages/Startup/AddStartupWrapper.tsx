6a41d18c7df174e1df6b41683b07d9a8802a6568
NaijaTechBro <bakisodiq@gmail.com>
2025-04-19 21:30:56 +0100

    update on dashboard

--- src/pages/Startup/AddStartupWrapper.tsx
+++ src/pages/Startup/AddStartupWrapper.tsx
@@ -9,14 +9,14 @@ const AddStartupWrapper: React.FC = () => {
 
   return (
     <div>
-      <div className="mb-6">
+      {/* <div className="mb-6">
         <h1 className="text-2xl font-bold">{isEditing ? 'Edit Startup' : 'Add New Startup'}</h1>
         <p className="text-gray-600">
           {isEditing 
             ? 'Update your startup information on GetListed' 
             : 'List your startup on the African Startup Directory'}
         </p>
-      </div>
+      </div> */}
       
       <StartupForm isEditing={isEditing} />
     </div>
